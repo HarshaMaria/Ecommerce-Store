d61236c2aa8eaf42e75ce7cdc226f34b365a1dbe
HarshaMaria <harsha.maria@edstem.com>
2023-11-06 22:46:37 +0530

    Frontend Done

--- ecommerce-store/src/reducers/userSlice.js
+++ ecommerce-store/src/reducers/userSlice.js
@@ -5,9 +5,7 @@ const userSlice = createSlice({
   initialState: null,
   reducers: {
     loginUser: (state, action) => {
-      // Handle user authentication logic and set the user data in the state
-      // For example, call your backend API to verify user credentials
-      return action.payload; // Set user data in the state upon successful login
+      return action.payload; 
     },
     logoutUser: () => null,
   },
