3e1bc04a3d036972d4ff93aad94a78202a79e29e
HarshaMaria <harsha.maria@edstem.com>
2023-11-06 12:05:05 +0530

    Doing

--- ecommerce-store/src/components/Cart.jsx
+++ ecommerce-store/src/components/Cart.jsx
@@ -14,23 +14,26 @@ const Cart = () => {
     dispatch(clearCart());
   };
 
+  const total = cartItems.reduce((acc, item) => acc + item.price, 0).toFixed(2);
+
   return (
     <div>
       <h2 className="text-4xl font-bold mb-4">Your Shopping Cart</h2>
       {cartItems.length === 0 ? (
         <p>Your Shopping cart is empty.</p>
       ) : (
-        <ul>
-          {cartItems.map(item => (
-            <li key={item.id}>
-              {item.name} - ${item.price}
-              <button onClick={() => handleRemoveFromCart(item)}>Remove</button>
-            </li>
-          ))}
-        </ul>
-      )}
-      {cartItems.length > 0 && (
-        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={handleClearCart}>Clear Cart</button>
+        <div>
+          <ul>
+            {cartItems.map(item => (
+              <li key={item.id}>
+                {item.name} - ${item.price}
+                <button onClick={() => handleRemoveFromCart(item)}>Remove</button>
+              </li>
+            ))}
+          </ul>
+          <p className="text-xl font-semibold mt-4">Total Price: ${total}</p>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2" onClick={handleClearCart}>Clear Cart</button>
+        </div>
       )}
     </div>
   );
