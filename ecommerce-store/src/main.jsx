001102cc8309eeea8e82fd86766dfdbe2276c98e
HarshaMaria <harsha.maria@edstem.com>
2023-11-06 17:08:29 +0530

    Doing

--- ecommerce-store/src/main.jsx
+++ ecommerce-store/src/main.jsx
@@ -1,21 +1,8 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import { Provider } from 'react-redux';
-// import App from './App.jsx'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { store } from './store'; // Import your Redux store
-import App from './App'; // Import your main App component
+import { store } from './store';
+import App from './App'; 
 import './index.css';
 
 ReactDOM.render(
