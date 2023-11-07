a8e760c9991ec049453fa77c9831189991e60926
HarshaMaria <harsha.maria@edstem.com>
2023-11-07 12:05:23 +0530

    Frontend Done

--- ecommerce-store/src/store.js
+++ ecommerce-store/src/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './reducers/cartSlice';
+import gamesReducer from './reducers/gamesSlice';
+import userReducer from './reducers/userSlice';
+import productsReducer from './reducers/productsSlice';
+import checkoutReducer from './reducers/checkoutSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     cart: cartReducer,
+    games: gamesReducer,
+    user: userReducer,
+    products: productsReducer,
+    checkout: checkoutReducer,
   },
 });
+
+export default store;
