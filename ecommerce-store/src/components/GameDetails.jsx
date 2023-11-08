cc9f791bf6cc2e7658177081ce241a0ab2b59303
HarshaMaria <harsha.maria@edstem.com>
2023-11-08 13:20:32 +0530

    ViewGameById

--- ecommerce-store/src/components/GameDetails.jsx
+++ ecommerce-store/src/components/GameDetails.jsx
@@ -1,35 +1,52 @@
-import React from 'react';
+import React, { useEffect ,useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../reducers/cartSlice';
 import { Link } from 'react-router-dom';
-import games from '../games';
+import axios from "axios";
+import Games from '../games';
 
 const GameDetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
-  const game = games.find((game) => game.id === parseInt(id));
+  // const game = games.find((game) => game.id === parseInt(id));
+  const [games, setGames] = useState({});
 
-  if (!game) {
+  if (!games) {
     return <div>Game not found!</div>;
   }
 
+  useEffect(() => {
+      // Fetch a specific game by ID from the API endpoint
+      axios.get(`http://localhost:8080/games/${id}`)
+      .then((response) => {
+        // console.log(response.data)
+        setGames(response.data)
+        console.log("Game by ID:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching game by ID:", error);
+      });
+  }, []);
+
+
   const handleAddToCart = () => {
-    dispatch(addToCart(game));
+    // dispatch(addToCart(game));
     alert('Game added to cart!');
   };
+
   const handleBuyNow = () => {
-    dispatch(buynow(game));
+    // dispatch(buynow(game));
     alert('Buy Now clicked! Implement Buy Now logic here.');
   };
 
 
   return (
     <div className="border p-4">
-      <h2 className="text-xl font-semibold mb-2">{game.name}</h2>
-      <img src={game.imageUrl} alt={game.name} className="mb-2 w-64 h-64 object-cover" />
-      <p className="text-gray-700 mb-2">{game.description}</p>
-      <p className="text-gray-700">Price: ${game.price}</p>
+      <h2 className="text-xl font-semibold mb-2">{games.name}</h2>
+      <img src={Games[id].imageUrl} alt={games.name} className="mb-2 w-64 h-64 object-cover" />
+      <p className="text-gray-700 mb-2">{games.description}</p>
+      <p className="text-gray-700">Price: ${games.price}</p>
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 flex" onClick={handleAddToCart}>
         Add to Cart
       </button>
