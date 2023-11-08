adf22fddf4d7470722beef72bb6f479ea06d37c5
HarshaMaria <harsha.maria@edstem.com>
2023-11-08 14:37:47 +0530

    Made Change

--- ecommerce-store/src/components/AddProductForm.jsx
+++ ecommerce-store/src/components/AddProductForm.jsx
@@ -1,4 +1,98 @@
+// import React, { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { addNewGame } from '../reducers/gamesSlice';
+
+// const AddProductForm = () => {
+//   const dispatch = useDispatch();
+//   const [formData, setFormData] = useState({
+//     name: '',
+//     description: '',
+//     price: '',
+//     imageUrl: '',
+//   });
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormData((prevData) => ({
+//       ...prevData,
+//       [name]: value,
+//     }));
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     dispatch(addNewGame(formData));
+//     setFormData({
+//       name: '',
+//       description: '',
+//       price: '',
+//       imageUrl: '',
+//     });
+//   };
+
+//   return (
+//     <div className="border p-4">
+//       <h2 className="text-xl font-semibold mb-4">Add New Product(Game)</h2>
+//       <form onSubmit={handleSubmit}>
+//         <div className="mb-4">
+//           <label className="block text-sm font-medium text-gray-600">Name</label>
+//           <input
+//             type="text"
+//             name="name"
+//             value={formData.name}
+//             onChange={handleChange}
+//             className="mt-1 p-2 border rounded w-full"
+//             required
+//           />
+//         </div>
+//         <div className="mb-4">
+//           <label className="block text-sm font-medium text-gray-600">Description</label>
+//           <textarea
+//             name="description"
+//             value={formData.description}
+//             onChange={handleChange}
+//             rows="4"
+//             className="mt-1 p-2 border rounded w-full"
+//             required
+//           />
+//         </div>
+//         <div className="mb-4">
+//           <label className="block text-sm font-medium text-gray-600">Price</label>
+//           <input
+//             type="number"
+//             name="price"
+//             value={formData.price}
+//             onChange={handleChange}
+//             className="mt-1 p-2 border rounded w-full"
+//             required
+//           />
+//         </div>
+//         <div className="mb-4">
+//           <label className="block text-sm font-medium text-gray-600">Image URL</label>
+//           <input
+//             type="url"
+//             name="imageUrl"
+//             value={formData.imageUrl}
+//             onChange={handleChange}
+//             className="mt-1 p-2 border rounded w-full"
+//             required
+//           />
+//         </div>
+//         <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+//           Add Product
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default AddProductForm;
+
+
+
+
 import React, { useState } from 'react';
+import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { addNewGame } from '../reducers/gamesSlice';
 
@@ -21,13 +115,23 @@ const AddProductForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addNewGame(formData));
-    setFormData({
-      name: '',
-      description: '',
-      price: '',
-      imageUrl: '',
-    });
+
+    axios.post('http://localhost:8080/cart/5/create', formData)
+      .then((response) => {
+        dispatch(addNewGame(response.data));
+
+        setFormData({
+          name: '',
+          description: '',
+          price: '',
+          imageUrl: '',
+        });
+
+        console.log('Product added successfully:', response.data);
+      })
+      .catch((error) => {
+        console.error('Error adding product:', error);
+      });
   };
 
   return (
--- ecommerce-store/src/components/GameDetails.jsx
+++ ecommerce-store/src/components/GameDetails.jsx
@@ -51,7 +51,7 @@ const GameDetails = () => {
         Add to Cart
       </button>
       <Link to="/Cart">
-      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded flex mt-2" onClick={handleBuyNow}>
+      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded flex mt-2" >
         View Cart
       </button>
       </Link>
