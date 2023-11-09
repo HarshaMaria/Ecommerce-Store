6f3b9aa474561539fcc5e0c93d3d3575bc389965
HarshaMaria <harsha.maria@edstem.com>
2023-11-09 16:16:00 +0530

    Made Changes

--- ecommerce-store/src/components/Home.jsx
+++ ecommerce-store/src/components/Home.jsx
@@ -8,7 +8,6 @@ const Home = () => {
   const [id, setId] = useState(0);
 
   useEffect(() => {
-    // Fetch all games from the API endpoint
     axios.get("http://localhost:8080/games")
       .then((response) => {
         setGames(response.data);
@@ -37,11 +36,11 @@ const Home = () => {
         ))}
       </div>
       <div className="fixed bottom-0 left-0 w-full bg-white p-4 text-center">
-        <Link to="/cart">
+        {/* <Link to="/cart">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             View Cart
           </button>
-        </Link>
+        </Link> */}
         <Link to="/add-product">
           <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 ml-4 rounded">
             Add Product
@@ -53,3 +52,77 @@ const Home = () => {
 };
 
 export default Home;
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+// import axios from "axios";
+// import Games from "../games";
+
+// const Home = () => {
+//   const [games, setGames] = useState([]);
+//   const [cartItems, setCartItems] = useState([]);
+
+//   useEffect(() => {
+//     // Fetch games from the API endpoint
+//     axios.get("http://localhost:8080/games")
+//       .then((response) => {
+//         setGames(response.data);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching games:", error);
+//       });
+
+//     // Fetch cart items from the API endpoint
+//     axios.get("http://localhost:8080/cart/all")
+//       .then((response) => {
+//         setCartItems(response.data);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching cart items:", error);
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       <div className="grid grid-cols-2 gap-4 w-100 mb-[60px]">
+//         {games && games.map((game) => (
+//           <div key={game.id} className="border p-4">
+//             <h2 className="text-xl font-semibold">{game.name}</h2>
+//             <img src={Games[game.id].imageUrl} alt={game.name} className="my-2 w-32 h-32 object-cover" />
+//             <p className="text-gray-700">{game.description}</p>
+//             <p className="text-gray-700">Price: ${game.price}</p>
+//             <Link to={`/game/${game.id}`}>
+//               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 flex">
+//                 View Game
+//               </button>
+//             </Link>
+//           </div>
+//         ))}
+//       </div>
+//       <div className="fixed bottom-0 left-0 w-full bg-white p-4 text-center">
+//         <Link to="/cart">
+//           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+//             View Cart 
+//             {/* ({cartItems.length}) */}
+//           </button>
+//         </Link>
+//         <Link to="/add-product">
+//           <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 ml-4 rounded">
+//             Add Product
+//           </button>
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
