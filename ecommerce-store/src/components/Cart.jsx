5968370b6d48626a72fa621b4fec5f9b444b39dd
HarshaMaria <harsha.maria@edstem.com>
2023-11-14 13:40:51 +0530

    Made Changes

--- ecommerce-store/src/components/Cart.jsx
+++ ecommerce-store/src/components/Cart.jsx
@@ -1,58 +1,147 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { removeFromCart, clearCart } from '../reducers/cartSlice';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
+
+// const Cart = () => {
+//   const [totalPrice, setTotalPrice] = useState(0)
+//   const cartItems = useSelector(state => state.cart);
+//   const dispatch = useDispatch();
+//   const [games, setGames] = useState([]);
+
+
+//   const [data, setData] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     axios.get('http://localhost:8081/cart/all')
+//       .then(response => {
+//         const array = response.data.map(item => item.gameId);
+//         const uniqueTargetIds = [...new Set(array)];
+
+//         setData(uniqueTargetIds);
+  
+//     axios.get('http://localhost:8081/games')
+//       .then(response => {
+//         const filteredGames = response.data.filter(game => uniqueTargetIds.includes(game.id));
+//         console.log(filteredGames)
+//         setGames(filteredGames);
+//         setTotalPrice(filteredGames.reduce((total, item) => total + item.price, 0));
+//         setIsLoading(false);
+//         })
+//         .catch(error => {
+
+//           setError(error);
+//           setIsLoading(false);
+//         });
+  
+//       })
+//       .catch(error => {
+//         setError(error);
+//       });
+//   }, []);
+  
+
+//   const handleRemoveFromCart = item => {
+//     axios.delete(`http://localhost:8081/cart/${item.id}`)
+//       .then(response => {
+//         dispatch(removeFromCart(item));
+//         console.log('Item removed from cart:', item);
+//       })
+//       .catch(error => {
+//         console.error('Error removing item from cart:', error);
+//       });
+//   };
+
+//   const handleClearCart = () => {
+//     dispatch(clearCart());
+//   };
+
+//   const total = cartItems.reduce((acc, item) => acc + item.price, 0).toFixed(2);
+
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div>Error: {error.message}</div>;
+//   }
+
+//   if (!data) {
+//     return <div>No data available.</div>;
+//   }
+
+
+//   return (
+//     <div>
+//       <h2 className="text-4xl font-bold mb-4">Your Shopping Cart</h2>
+//       {games.length === 0 ? (
+//         <div>
+//           <p>Your Shopping cart is empty.</p>
+//           <Link to="/home">
+//             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">
+//               Back to Home
+//             </button>
+//           </Link>
+//         </div>
+//       ) : (
+//         <div>
+//       <div>
+//         {games.map(game => (
+//           <div key={game.id} className="game-item">
+//             <h3>{game.name}</h3>
+//             <p>{game.description}</p>
+//             <p>Price: ${game.price}</p>
+//             <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4 mb-4" onClick={() => handleRemoveFromCart(games.id)}>
+//             Remove
+//           </button>
+//           </div>
+//         ))}
+//       </div>
+//           <p className="text-xl font-semibold mt-4">Total Price: ${totalPrice}</p>
+
+//           <Link to="/checkout">
+//             <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2 flex">
+//               Proceed to Checkout
+//             </button>
+//           </Link>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Cart;
+
+
+
+
+
+
+
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeFromCart, clearCart } from '../reducers/cartSlice';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const Cart = () => {
-  const [totalPrice, setTotalPrice] = useState(0)
   const cartItems = useSelector(state => state.cart);
   const dispatch = useDispatch();
-  const [games, setGames] = useState([]);
 
-
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    axios.get('http://localhost:8081/cart/all')
-      .then(response => {
-        const array = response.data.map(item => item.gameId);
-        const uniqueTargetIds = [...new Set(array)];
-
-        setData(uniqueTargetIds);
-  
-    axios.get('http://localhost:8081/games')
-      .then(response => {
-        const filteredGames = response.data.filter(game => uniqueTargetIds.includes(game.id));
-        console.log(filteredGames)
-        setGames(filteredGames);
-        setTotalPrice(filteredGames.reduce((total, item) => total + item.price, 0));
-        setIsLoading(false);
-        })
-        .catch(error => {
-
-          setError(error);
-          setIsLoading(false);
-        });
-  
-      })
-      .catch(error => {
-        setError(error);
-      });
-  }, []);
-  
-
-  const handleRemoveFromCart = item => {
-    axios.delete(`http://localhost:8081/cart/${item.id}`)
-      .then(response => {
+  const handleRemoveFromCart = async item => {
+    try {
+      const response = await axios.delete(`http://localhost:8080/games/carts/${item.id}`);
+      if (response.status === 200) {
         dispatch(removeFromCart(item));
-        console.log('Item removed from cart:', item);
-      })
-      .catch(error => {
-        console.error('Error removing item from cart:', error);
-      });
+      } else {
+        console.error('Failed to remove item from cart');
+      }
+    } catch (error) {
+      console.error('Error removing item from cart:', error);
+    }
   };
 
   const handleClearCart = () => {
@@ -61,51 +150,38 @@ const Cart = () => {
 
   const total = cartItems.reduce((acc, item) => acc + item.price, 0).toFixed(2);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  if (!data) {
-    return <div>No data available.</div>;
-  }
-
-
   return (
     <div>
       <h2 className="text-4xl font-bold mb-4">Your Shopping Cart</h2>
-      {games.length === 0 ? (
+      {cartItems.length === 0 ? (
         <div>
-          <p>Your Shopping cart is empty.</p>
-          <Link to="/home">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">
-              Back to Home
-            </button>
-          </Link>
-        </div>
+        <p>Your Shopping cart is empty.</p>
+        <Link to="/home">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">
+            Back to Home
+          </button>
+        </Link>
+      </div>
       ) : (
         <div>
-      <div>
-        {games.map(game => (
-          <div key={game.id} className="game-item">
-            <h3>{game.name}</h3>
-            <p>{game.description}</p>
-            <p>Price: ${game.price}</p>
-            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4 mb-4" onClick={() => handleRemoveFromCart(games.id)}>
-            Remove
+          <ul>
+            {cartItems.map(item => (
+              <li key={item.id}>
+                {item.name} - ${item.price}
+                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-4 ml-4" onClick={() => handleRemoveFromCart(item)}>
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+          <p className="text-xl font-semibold mt-4">Total Price: ${total}</p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2" onClick={handleClearCart}>
+            Clear Cart
           </button>
-          </div>
-        ))}
-      </div>
-          <p className="text-xl font-semibold mt-4">Total Price: ${totalPrice}</p>
-
           <Link to="/checkout">
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2 flex">
-              Proceed to Checkout
-            </button>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2 flex">
+            Proceed to Checkout
+          </button>
           </Link>
         </div>
       )}
--- ecommerce-store/src/components/GameDetails.jsx
+++ ecommerce-store/src/components/GameDetails.jsx
@@ -1,3 +1,92 @@
+// import React, { useEffect, useState } from 'react';
+// import { useParams, Link } from 'react-router-dom';
+// import Games from '../games';
+// import axios from "axios";
+
+// const GameDetails = () => {
+//   const { id } = useParams();
+//   const [game, setGame] = useState({});
+//   const [cartItems, setCartItems] = useState([]);
+//   const [cartId, setCartId] = useState();
+  
+//   const userId = 24;
+
+//   useEffect(() => {
+//     // Fetch game details from the API endpoint
+//     axios.get(`http://localhost:8081/games/${id}`)
+//       .then((response) => {
+//         setGame(response.data);
+//         console.log("Game by ID:", response.data);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching game details:", error);
+//       });
+
+//     axios.get(`http://localhost:8081/cart/all`)
+//       .then((response) => {
+//         setCartItems(response.data);
+//         console.log('Cart items:', response.data);  
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching cart items:", error);
+//       });
+//   }, [id]);
+
+//   const handleAddToCart = () => {
+//     axios.post('http://localhost:8081/cart/create')
+//       .then(createCartResponse => {
+//         const cartId = createCartResponse.data;
+//         axios.post(`http://localhost:8081/cart/${cartId}/addGame`, {
+//           "gameId": id,
+//           "count": 1
+//         })
+//         .then(response => {
+//           console.log('Game added to cart:', response.data);
+//           alert('Game added to cart!');
+//         })
+//         .catch(error => {
+//           console.error('Error adding game to cart:', error);
+//         });
+//       })
+//       .catch(error => {
+//         console.error('Error creating cart:', error);
+//       });
+//   };
+  
+//   const handleBuyNow = () => {
+//     alert('Buy Now clicked! Implement Buy Now logic here.');
+//   };
+
+//   return (
+//     <div className="border p-4">
+//       <h2 className="text-xl font-semibold mb-2">{game.name}</h2>
+//       <img src={Games[id].imageUrl} alt={game.name} className="mb-2 w-64 h-64 object-cover" />
+//       <p className="text-gray-700 mb-2">{game.description}</p>
+//       <p className="text-gray-700">Price: ${game.price}</p>
+//       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex mt-2" onClick={handleAddToCart}>
+//         Add to Cart
+//       </button>
+//       <Link to="/cart">
+//         <button className="bg-green-500 hover-bg-green-700 text-white font-bold py-2 px-4 rounded flex mt-2">
+//           View Cart
+//         </button>
+//       </Link>
+//       <Link to="/checkout">
+//       <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2" onClick={handleBuyNow}>
+//         Buy Now
+//       </button>
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default GameDetails;
+
+
+
+
+
+
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import Games from '../games';
@@ -7,10 +96,7 @@ const GameDetails = () => {
   const { id } = useParams();
   const [game, setGame] = useState({});
   const [cartItems, setCartItems] = useState([]);
-  const [cartId, setCartId] = useState();
   
-  const userId = 24;
-
   useEffect(() => {
     // Fetch game details from the API endpoint
     axios.get(`http://localhost:8081/games/${id}`)
@@ -22,7 +108,8 @@ const GameDetails = () => {
         console.error("Error fetching game details:", error);
       });
 
-    axios.get(`http://localhost:8081/cart/all`)
+    // View Cart
+    axios.get(`http://localhost:8081/games/carts`)
       .then((response) => {
         setCartItems(response.data);
         console.log('Cart items:', response.data);  
@@ -33,24 +120,18 @@ const GameDetails = () => {
   }, [id]);
 
   const handleAddToCart = () => {
-    axios.post('http://localhost:8081/cart/create')
-      .then(createCartResponse => {
-        const cartId = createCartResponse.data;
-        axios.post(`http://localhost:8080/cart/${cartId}/addGame`, {
-          "gameId": id,
-          "count": 1
-        })
-        .then(response => {
-          console.log('Game added to cart:', response.data);
-          alert('Game added to cart!');
-        })
-        .catch(error => {
-          console.error('Error adding game to cart:', error);
-        });
-      })
-      .catch(error => {
-        console.error('Error creating cart:', error);
-      });
+    // Add the game to Cart
+    axios.post(`http://localhost:8080/games/${id}/create`, {
+      "gameId": id,
+      "count": 1
+    })
+    .then(response => {
+      console.log('Game added to cart:', response.data);
+      alert('Game added to cart!');
+    })
+    .catch(error => {
+      console.error('Error adding game to cart:', error);
+    });
   };
   
   const handleBuyNow = () => {
@@ -67,14 +148,14 @@ const GameDetails = () => {
         Add to Cart
       </button>
       <Link to="/cart">
-        <button className="bg-green-500 hover-bg-green-700 text-white font-bold py-2 px-4 rounded flex mt-2">
+        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded flex mt-2">
           View Cart
         </button>
       </Link>
       <Link to="/checkout">
-      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2" onClick={handleBuyNow}>
-        Buy Now
-      </button>
+        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2" onClick={handleBuyNow}>
+          Buy Now
+        </button>
       </Link>
     </div>
   );
