3e3a0056ebb115697d3ccd775ce44c022bdf2742
HarshaMaria <harsha.maria@edstem.com>
2023-11-15 11:21:42 +0530

    Made Change

--- ecommerce-store/src/components/Cart.jsx
+++ ecommerce-store/src/components/Cart.jsx
@@ -121,21 +121,115 @@
 
 
 
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+// import React from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { removeFromCart, clearCart } from '../reducers/cartSlice';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
+
+// const Cart = () => {
+//   const cartItems = useSelector(state => state.cart);
+//   const dispatch = useDispatch();
+
+//   const handleRemoveFromCart = async item => {
+//     try {
+//       const response = await axios.delete(`http://localhost:8080/games/carts/${item.id}`);
+//       if (response.status === 200) {
+//         dispatch(removeFromCart(item));
+//       } else {
+//         console.error('Failed to remove item from cart');
+//       }
+//     } catch (error) {
+//       console.error('Error removing item from cart:', error);
+//     }
+//   };
+
+//   const handleClearCart = () => {
+//     dispatch(clearCart());
+//   };
+
+//   const total = cartItems.reduce((acc, item) => acc + item.price, 0).toFixed(2);
+
+//   return (
+//     <div>
+//       <h2 className="text-4xl font-bold mb-4">Your Shopping Cart</h2>
+//       {cartItems.length === 0 ? (
+//         <div>
+//         <p>Your Shopping cart is empty.</p>
+//         <Link to="/home">
+//           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">
+//             Back to Home
+//           </button>
+//         </Link>
+//       </div>
+//       ) : (
+//         <div>
+//           <ul>
+//             {cartItems.map(item => (
+//               <li key={item.id}>
+//                 {item.name} - ${item.price}
+//                 <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-4 ml-4" onClick={() => handleRemoveFromCart(item)}>
+//                   Remove
+//                 </button>
+//               </li>
+//             ))}
+//           </ul>
+//           <p className="text-xl font-semibold mt-4">Total Price: ${total}</p>
+//           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2" onClick={handleClearCart}>
+//             Clear Cart
+//           </button>
+//           <Link to="/checkout">
+//           <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2 flex">
+//             Proceed to Checkout
+//           </button>
+//           </Link>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Cart;
+
+
+
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { removeFromCart, clearCart } from '../reducers/cartSlice';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const Cart = () => {
-  const cartItems = useSelector(state => state.cart);
+  const [cartItems, setCartItems] = useState([]);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Fetch cart items from the API endpoint
+    axios.get(`http://localhost:8081/games/carts`)
+      .then((response) => {
+        setCartItems(response.data);
+        console.log('Cart items:', response.data);  
+      })
+      .catch((error) => {
+        console.error("Error fetching cart items:", error);
+      });
+  }, []);
 
   const handleRemoveFromCart = async item => {
     try {
-      const response = await axios.delete(`http://localhost:8080/games/carts/${item.id}`);
+      console.log(item.id)
+      const response = await axios.delete(`http://localhost:8081/games/carts/${item.id}`);
       if (response.status === 200) {
         dispatch(removeFromCart(item));
+        // Fetch updated cart items
+        axios.get(`http://localhost:8081/games/carts`)
+          .then((response) => {
+            setCartItems(response.data);
+            console.log('Updated cart items:', response.data);  
+          })
+          .catch((error) => {
+            console.error("Error fetching updated cart items:", error);
+          });
       } else {
         console.error('Failed to remove item from cart');
       }
@@ -167,17 +261,17 @@ const Cart = () => {
           <ul>
             {cartItems.map(item => (
               <li key={item.id}>
-                {item.name} - ${item.price}
-                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-4 ml-4" onClick={() => handleRemoveFromCart(item)}>
+                {item.game.name} - ${item.game.price}
+                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-4 ml-4" onClick={() => handleRemoveFromCart(item.game)}>
                   Remove
                 </button>
               </li>
             ))}
           </ul>
           <p className="text-xl font-semibold mt-4">Total Price: ${total}</p>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2" onClick={handleClearCart}>
+          {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2" onClick={handleClearCart}>
             Clear Cart
-          </button>
+          </button> */}
           <Link to="/checkout">
           <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2 flex">
             Proceed to Checkout
