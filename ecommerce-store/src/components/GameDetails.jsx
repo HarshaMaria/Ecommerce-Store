94dd7a5c07fd7e91e10bad1be27bb464b825974f
HarshaMaria <harsha.maria@edstem.com>
2023-11-20 09:43:21 +0530

    Made Changes

--- ecommerce-store/src/components/GameDetails.jsx
+++ ecommerce-store/src/components/GameDetails.jsx
@@ -53,7 +53,7 @@ const GameDetails = () => {
    <div className="flex justify-center items-center mt-12"> 
     <div className="border p-8 w-80 bg-rose-200">
       <h2 className="text-xl font-semibold mb-2">{game.name}</h2>
-      <img src={Games[id].imageUrl} alt={game.name} className="mb-2 w-64 h-64 object-cover" />
+      <img src={Games[id-1].imageUrl} alt={game.name} className="mb-2 w-64 h-64 object-cover" />
       <p className="text-gray-700 mb-2">{game.description}</p>
       <p className="text-gray-700">Price: ${game.price}</p>
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex mt-2" onClick={handleAddToCart}>
