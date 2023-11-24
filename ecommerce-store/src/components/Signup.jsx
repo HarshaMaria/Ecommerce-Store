85c1136937913e060d3e7b0a4699e48e8e3e3558
HarshaMaria <harsha.maria@edstem.com>
2023-11-24 10:14:46 +0530

    Made Change

--- ecommerce-store/src/components/Signup.jsx
+++ ecommerce-store/src/components/Signup.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-
  
 export const Signup = (props) => {
   const [email, setEmail] = useState("");
