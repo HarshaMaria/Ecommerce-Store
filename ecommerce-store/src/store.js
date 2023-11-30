6118c0da790162d43e7ac60560cecbe4bf1b4725
HarshaMaria <harsha.maria@edstem.com>
2023-11-30 10:41:46 +0530

    Made Change

--- ecommerce-store/src/store.js
+++ ecommerce-store/src/store.js
@@ -3,7 +3,6 @@ import cartReducer from './reducers/cartSlice';
 import gamesReducer from './reducers/gamesSlice';
 import userReducer from './reducers/userSlice';
 import productsReducer from './reducers/productsSlice';
-import checkoutReducer from './reducers/checkoutSlice';
 
 const store = configureStore({
   reducer: {
@@ -11,7 +10,6 @@ const store = configureStore({
     games: gamesReducer,
     user: userReducer,
     products: productsReducer,
-    checkout: checkoutReducer,
   },
 });
 
