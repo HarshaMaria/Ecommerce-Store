5c6324cdbd4c6ad3222b9231065d885ebed65fae
HarshaMaria <harsha.maria@edstem.com>
2023-12-06 09:48:27 +0530

    Made Changes

--- ecommerce-store/src/components/Cart.jsx
+++ ecommerce-store/src/components/Cart.jsx
@@ -1,88 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { removeFromCart, clearCart } from '../reducers/cartSlice';
-import { Link,useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, Link } from 'react-router-dom';
+import { fetchCartItems, removeFromCart } from '../reducers/cartSlice';
 
 const Cart = () => {
-  const [cartItems, setCartItems] = useState([]);
   const dispatch = useDispatch();
-  const [total, setTotal] = useState(null)
   const { userId } = useParams();
+  const cartItems = useSelector((state) => state.cart);
+  console.log(cartItems)
+  const total = cartItems.reduce((total, item) => total + item.price * item.count, 0);
 
   useEffect(() => {
-    axios.get(`http://localhost:8081/carts?userId=${userId}`)
-      .then((response) => {
-        setCartItems(response.data);
-        const totalPrices = response.data.reduce((total, item) => {
-          const itemTotalPrice = item.price * item.count;
-          return total + itemTotalPrice; 
-        }, 0);
-        setTotal(totalPrices);
-      })
-      .catch((error) => {
-        console.error("Error fetching cart items:", error);
-      });
-  }, []);
+    dispatch(fetchCartItems(userId));
+  }, [dispatch, userId]);
 
-  const handleRemoveFromCart = async item => {
-    try {
-      const response = await axios.delete(`http://localhost:8081/carts/${item.gameId}?userId=${userId}`);
-      if (response.status === 200) {
-        dispatch(removeFromCart(item));
-        window.location.reload();
-        // Updated cart
-        axios.get(`http://localhost:8081/games/carts?userId=${userId}`)
-          .then((response) => {
-            setCartItems(response.data);
-            setTotal(0);
-          })
-          .catch((error) => {
-            console.error("Error fetching updated cart items:", error);
-          });
-      } else {
-        console.error('Failed to remove item from cart');
-      }
-    } catch (error) {
-      console.error('Error removing item from cart:', error);
-    }
+  const handleRemoveFromCart = (item) => {
+    // console.log(item)
+    dispatch(removeFromCart({ gameId: item.gameId, userId }));
+    const cart = cartItems.filter(game => game.gameId !== item.gameId)
+    
   };
 
   return (
-   <div className="mt-[24px]"> 
-    <div className="border p-4 bg-purple-200 w-[1000px]">
-      <h2 className="text-4xl font-bold mb-4">Your Shopping Cart</h2>
-      {cartItems.length === 0 ? (
-        <div>
-        <p>Your Shopping cart is empty.</p>
-        <Link to={`/home/${userId}`}>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">
-            Back to Home
-          </button>
-        </Link>
+    <div className="mt-[24px]"> 
+      <div className="border p-4 bg-purple-200 w-[1000px]">
+        <h2 className="text-4xl font-bold mb-4">Your Shopping Cart</h2>
+        {cartItems.length === 0 ? (
+          <div>
+            <p>Your Shopping cart is empty.</p>
+            <Link to={`/home/${userId}`}>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">
+                Back to Home
+              </button>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <ul>
+              {cartItems && cartItems.map((item) => (
+                <div key={item.gameId}>
+                  <p>{item.name} - ${item.price}</p>
+                  <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-4 ml-4" onClick={() => handleRemoveFromCart(item)}>
+                    Remove
+                  </button>
+                </div>
+              ))} 
+            </ul>
+            <p className="text-xl font-semibold mt-4">Total Price: ${total}</p>
+            <Link to={`/home/${userId}`}>
+              <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2 flex">
+                Proceed to Checkout
+              </button>
+            </Link>
+          </div>
+        )}
       </div>
-      ) : (
-        <div>
-          <ul>
-          {cartItems && cartItems.map((item) => (
-        <div key={item.gameId}>
-          <p>{item.name} - ${item.price}</p>
-          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-4 ml-4" onClick={() => handleRemoveFromCart(item)}>
-            Remove
-          </button>
-        </div>
-      ))} 
-          </ul>
-          <p className="text-xl font-semibold mt-4">Total Price: ${total}</p>
-          <Link to={`/home/${userId}`}>
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2 flex">
-            Proceed to Checkout
-          </button>
-          </Link>
-        </div>
-      )}
-    </div>
-   </div> 
+    </div>  
   );
 };
 
--- ecommerce-store/src/reducers/cartSlice.js
+++ ecommerce-store/src/reducers/cartSlice.js
@@ -1,19 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-export const cartSlice = createSlice({
+export const fetchCartItems = createAsyncThunk(
+  'cart/fetchCartItems',
+  async (userId) => {
+    const response = await axios.get(`http://localhost:8081/carts?userId=${userId}`);
+    return response.data;
+  }
+);
+
+export const removeFromCart = createAsyncThunk(
+  'cart/removeFromCart',
+  async ({ gameId, userId }) => {
+    const response = await axios.delete(`http://localhost:8081/carts/${gameId}?userId=${userId}`);
+    return gameId;
+  }
+);
+
+const cartSlice = createSlice({
   name: 'cart',
   initialState: [],
   reducers: {
-    addToCart: (state, action) => {
-      state.push(action.payload);
-    },
-    removeFromCart: (state, action) => {
-      return state.filter(item => item.id !== action.payload.id);
-    },
     clearCart: () => [],
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCartItems.fulfilled, (state, action) => {
+        return action.payload;
+      })
+      .addCase(removeFromCart.fulfilled, (state, action) => {
+        return state.filter(item => item.id !== action.payload.id);
+      });
   },
 });
 
-export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
+export const { clearCart } = cartSlice.actions;
 export default cartSlice.reducer;
-
