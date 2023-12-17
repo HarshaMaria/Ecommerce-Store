41828a4050b3e90fa576caf4abc911203022d37d
HarshaMaria <harsha.maria@edstem.com>
2023-12-17 11:13:54 +0530

    Made Changes

--- ecommerce-store/src/reducers/productsSlice.js
+++ ecommerce-store/src/reducers/productsSlice.js
@@ -5,7 +5,6 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const addProduct = createAsyncThunk(
   'products/addProduct',
   async ({product,token}) => {
-    console.log(product,token)
     const response = await axios.post(`${BASE_URL}/v1/games`, product, { 
       headers:{
         Authorization: `Bearer ${token}`
